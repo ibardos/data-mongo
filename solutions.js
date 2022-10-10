b78773e2ec413a73affd99a78d8a3d665d624edd
csesznegid <csesznegid@gmail.com>
2022-10-10 21:27:15 +0200

    chore: add file for the solutions

---
+++ solutions.js
@@ -0,0 +1,18 @@
+/**
+ * Count orders with a quantity greater than 10
+ */
+
+
+/**
+ * What are the top 3 most selling products (based on sold quantity)?
+ */
+
+
+/**
+ * Which city sold the most products?
+ */
+
+
+/**
+ * What was the best month for sales (based on product prices)?
+ */
